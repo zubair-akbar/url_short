ba87cc117858e38f5669155edaab7502987f9578
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2023-03-06 16:14:15 -0500

    Increase model detail

--- database/model/model.js
+++ database/model/model.js
@@ -2,9 +2,23 @@ import mongoose from 'mongoose';
 import db from '../../database/index.js';
 
 const urlSchema = new mongoose.Schema({
-  originalURL: String,
-  shortUrl: String,
-  timesUsed: Number
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  clicks: {
+    type: Number,
+    default: 0
+  }
 });
 
 export default mongoose.model("Url", urlSchema);
