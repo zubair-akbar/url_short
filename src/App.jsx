504814ff70f2b324da23e9e588d8764ab3d8affd
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2023-03-08 14:04:06 -0500

    First pass error handling + URL type validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [originalURL, setOriginalURL] = useState('')
+  const [originalUrl, setOriginalUrl] = useState('')
   const [customURL, setCustomURL] = useState('')
-  const [load, setLoad] = useState(false);
+  const [loadState, setLoadState] = useState(false);
   const [shortUrl, setShortUrl] = useState('')
+  const [errorState, setErrorState] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setOriginalURL(e.target.originalUrl.value)
+    setOriginalUrl(e.target.originalUrl.value)
     setCustomURL(e.target.customUrl.value)
+
+    try {
+      const urlPattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+      if (!urlPattern.test(originalUrl)) {
+        throw new Error('Invalid URL');
+      }
+    } catch (error) {
+      setErrorState(error.message);
+    }
+    setLoadState(true)
     shortUrlHash()
   }
 
@@ -25,7 +36,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>URL Shortener</h1>
+      <h1>LinkDwarf</h1>
           <form onSubmit={handleSubmit}>
             <label>Enter your original URL here:</label>
             <input type='text' name="originalUrl"></input>
@@ -35,7 +46,12 @@ const App = () => {
             <br/>
             <button type="submit">Submit</button>
           </form>
-
+          {errorState && (
+            <div className="error">{errorState}</div>
+          )}
+          {loadState && !errorState &&
+            <div>Your custom URL is <a href={window.location.origin + '/' +shortUrl}>{window.location.origin + '/' +shortUrl}!</a></div>
+          }
     </div>
   )
 }
