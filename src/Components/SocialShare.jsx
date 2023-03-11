5da042c6e1776113d068d856c25a5e74241c7f63
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2023-03-10 20:53:18 -0500

    Styling edits

--- src/Components/SocialShare.jsx
+++ src/Components/SocialShare.jsx
@@ -30,14 +30,14 @@ const SocialShareWrapper = styled.div`
 const SocialShareButton = styled.button`
   padding: 10px;
   margin: 10px;
-  background: linear-gradient(to bottom, #926444, #c48f60);
+  background: #586151;
   border: none;
   border-radius: 10px;
   color: #fff;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   &:hover {
-    background-color: #4fa4db;
+    background-color: #939e8a;
   }
   &:focus {
     box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1px rgba(0, 0, 0, 0.1), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5);
