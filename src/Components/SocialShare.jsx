7eabbd23b9e45bf601ca7f9d584927a69f82b2bd
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2023-03-10 19:13:01 -0500

    Social media share component

---
+++ src/Components/SocialShare.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const SocialShare = ({ url }) => {
+  const shareOnTwitter = () => {
+    const tweetText = encodeURIComponent(`Check out this link: ${url}`);
+    window.open(`https://twitter.com/intent/tweet?text=${tweetText}`);
+  };
+
+  const shareOnFacebook = () => {
+    const encodedUrl = encodeURIComponent(url);
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`);
+  };
+
+  return (
+    <SocialShareWrapper>
+      <SocialShareButton onClick={shareOnTwitter}>Share on Twitter</SocialShareButton>
+      <SocialShareButton onClick={shareOnFacebook}>Share on Facebook</SocialShareButton>
+    </SocialShareWrapper>
+  );
+};
+
+const SocialShareWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const SocialShareButton = styled.button`
+  padding: 10px;
+  margin: 10px;
+  background: linear-gradient(to bottom, #926444, #c48f60);
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: #4fa4db;
+  }
+  &:focus {
+    box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1px rgba(0, 0, 0, 0.1), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5);
+    outline: 0;
+  }
+`;
+
+export default SocialShare;
