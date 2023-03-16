8e1a56cfac3e58ad5a4d120ae475cb7fb92a1d9c
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2023-03-16 14:43:45 -0400

    Updated nightmode to socialshare component

--- src/Components/SocialShare.jsx
+++ src/Components/SocialShare.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const SocialShare = ({ url }) => {
+export const SocialShare = ({ url, theme }) => {
   const shareOnTwitter = () => {
     const tweetText = encodeURIComponent(`Check out this link: ${url}`);
     window.open(`https://twitter.com/intent/tweet?text=${tweetText}`);
@@ -30,14 +30,14 @@ const SocialShareWrapper = styled.div`
 const SocialShareButton = styled.button`
   padding: 10px;
   margin: 10px;
-  background: #586151;
+  background: var(--button-hover);
   border: none;
   border-radius: 10px;
   color: #fff;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   &:hover {
-    background-color: #939e8a;
+    background-color: var(--button-hover);
   }
   &:focus {
     box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1px rgba(0, 0, 0, 0.1), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5);
